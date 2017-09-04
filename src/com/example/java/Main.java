a7af520f6c6176f13709bb535fb614ed84c32b18
kkruse13 <kkruse13@students.mchenry.edu>
2017-09-03 20:42:34 -0600

    Initial commit

---
+++ src/com/example/java/Main.java
@@ -0,0 +1,14 @@
+package com.example.java;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hi my name is Kevin.");
+        System.out.println("I am starting a course on Java Programming.");
+        System.out.println("I will learn many new ways to code.");
+        System.out.println("I am excited to be able to code for Android.");
+        System.out.println("Sometime soon I hope to develop an app.");
+    }
+}
